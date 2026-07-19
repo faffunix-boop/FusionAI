dbfa5423b0a51726dabb21824e1b1149bae4736a
faffunix-boop <faffunix@gmail.com>
2026-07-19 23:30:39 +0800

    Update planner.js

--- backend/pipeline/planner.js
+++ backend/pipeline/planner.js
@@ -1,1 +1,17 @@
+const logger = require("../utils/logger");
 
+async function planner(data) {
+  logger.info("Planner", "Creating execution plan...");
+
+  return {
+    ...data,
+    plan: [
+      "Analyze request",
+      "Generate solution",
+      "Review result",
+      "Validate output"
+    ]
+  };
+}
+
+module.exports = planner;
