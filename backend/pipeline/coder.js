f23b1af209924414b64e838c3ecca930d063a7d2
faffunix-boop <faffunix@gmail.com>
2026-07-19 23:31:03 +0800

    Update coder.js

--- backend/pipeline/coder.js
+++ backend/pipeline/coder.js
@@ -1,1 +1,12 @@
+const logger = require("../utils/logger");
 
+async function coder(data) {
+  logger.info("Coder", "Generating code...");
+
+  return {
+    ...data,
+    response: null // nanti ganti dengan panggilan AI sebenar
+  };
+}
+
+module.exports = coder;
