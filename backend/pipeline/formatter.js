0867000d817dd0d6b75e608d78aa16fe5a20a013
faffunix-boop <faffunix@gmail.com>
2026-07-19 23:32:12 +0800

    Update formatter.js

--- backend/pipeline/formatter.js
+++ backend/pipeline/formatter.js
@@ -1,1 +1,9 @@
+const logger = require("../utils/logger");
 
+async function formatter(data) {
+  logger.info("Formatter", "Formatting response...");
+
+  return data.response;
+}
+
+module.exports = formatter;
